a95246448afe7d76160f4827fe7c484f8600b5a3
ahmedabidi7 <abidi.ahmed.j@gmail.com>
2023-05-09 13:34:12 +0100

    commit

---
+++ Week2/Day 2/Core/button clicker/script.js
@@ -0,0 +1,16 @@
+function loginf(e){
+    if (e.innerText=="Logout"){
+        e.innerText="Login"
+    }
+    else {
+        e.innerText="Logout"
+    }
+}
+
+function remoov(e){
+    e.remove();
+}
+
+function alrr(){
+    alert("Ninja was liked");
+}
---
+++ Week2/Day 2/Practice/video preview/script.js
@@ -0,0 +1,7 @@
+console.log("page loaded...");
+function playy(e){
+    
+    console.log(e);
+    e.muted = true;
+    e.play();
+}
