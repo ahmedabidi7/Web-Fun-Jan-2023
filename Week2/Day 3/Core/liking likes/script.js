25d4f61f4cd2ff12b038699da4d176c6f30e2b12
ahmedabidi7 <abidi.ahmed.j@gmail.com>
2023-05-09 15:48:54 +0100

    commit

---
+++ Week2/Day 3/Core/liking likes/script.js
@@ -0,0 +1,15 @@
+var x= document.querySelector("cont1")
+var y= document.querySelector("cont2")
+var z= document.querySelector("cont3")
+function like1(){
+    console.log(x);
+    x.innerHTML++;
+}
+function like2(){
+    console.log(y);
+    y.innerHTML++;
+}
+function like3(){
+    console.log(z);
+    z.innerHTML++;
+}
